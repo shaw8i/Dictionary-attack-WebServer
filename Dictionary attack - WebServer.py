aad63cb225562c3d24ce726be17f2296244c6af1
Shawqi <31892852+shaw8i@users.noreply.github.com>
2017-10-20 11:19:09 +0300

    Add files via upload

---
+++ Dictionary attack - WebServer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2.7
+
+import urllib.request,sys
+
+host = sys.argv[1]
+file_name = sys.argv[2]
+
+file = open(file_name,"r")
+data = file.readlines()
+
+for line in data:
+    word = line.strip("\n")
+    strreq = "{0}/{1}".format(host,word)
+    try:
+        req = urllib2.urlopen(strreq)
+        if req.code == 200:
+            print("[+] {0}/{1} is Found (200)".format(host,word))
+    except:
+         print("[-] {0}/{1} is Not Found (404)".format(host,word))
